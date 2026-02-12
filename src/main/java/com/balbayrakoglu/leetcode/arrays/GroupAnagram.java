bc3a6d72717bf0b0da7be8817a06aedc5fc3d89c
Burak Albayrakoglu <balbayrakoglu@gmail.com>
2026-02-12 15:54:03 +0100

    refactoring

--- src/main/java/com/balbayrakoglu/leetcode/arrays/GroupAnagram.java
+++ src/main/java/com/balbayrakoglu/leetcode/arrays/GroupAnagram.java
@@ -7,7 +7,7 @@ public class GroupAnagram {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         if (strs == null || strs.length == 0) {
-            return new ArrayList<List<String>>();
+            return new ArrayList<>();
         }
 
         Map<String, List<String>> sortedMap = new HashMap<>();
